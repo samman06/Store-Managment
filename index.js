5e7b84cdbc44e6817c1491d42d7b0094fba6e4a4
Mohammed-Elsamman <monem7242@gmail.com>
2020-06-20 05:47:17 +0200

    deploye step #1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
 const keys = require('./configs/keys');
+const path = require('path');
 
 const department = require('./routes/Department');
 const promotion = require('./routes/Promotion');
@@ -31,6 +32,14 @@ app.use('/departments', department);
 app.use('/promotions', promotion);
 // products router
 app.use('/products', product);
+
+//server static assets if in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
+    })
+}
 
 app.listen(PORT, (req, res) => {
     console.log("server running on port: " + PORT);
