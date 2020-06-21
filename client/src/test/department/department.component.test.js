89b083ae426a609fe56511df70fb8bb6b52b15bd
Mohammed-Elsamman <monem7242@gmail.com>
2020-06-21 05:59:46 +0200

    refactor testing code

--- client/src/test/department/department.component.test.js
+++ client/src/test/department/department.component.test.js
@@ -4,35 +4,35 @@ import AddDepartmentModal from "../../components/Department/addDepartmentModal"
 import EditDepartmentModal from "../../components/Department/editDepartmentModal"
 import DepartmentItem from "../../components/Department/departmentItem"
 
-describe("expect Department shallow", () => {
-    it("expect DepartmentItem shallow", () => {
+describe("Test Department shallow", () => {
+    it("should DepartmentItem length be 1", () => {
         expect(shallow(<DepartmentItem departments={[]}/>).length).toEqual(1);
     });
-    it("expect EditDepartmentModal shallow", () => {
+    it("should EditDepartmentModal length be 1", () => {
         expect(shallow(<EditDepartmentModal errors={{}}/>).length).toEqual(1);
     });
-    it("expect AddDepartmentModal shallow", () => {
+    it("should AddDepartmentModal length be 1", () => {
         expect(shallow(<AddDepartmentModal errors={{}}/>).length).toEqual(1);
     })
 });
 
-describe("expect Department Snapshot", () => {
-    it("expect DepartmentItem Snapshot", () => {
+describe("Test Department Snapshot", () => {
+    it("Take DepartmentItem Snapshot", () => {
         expect(shallow(<DepartmentItem departments={[]}/>)).toMatchSnapshot();
     });
-    it("expect EditDepartmentModal Snapshot", () => {
+    it("Take EditDepartmentModal Snapshot", () => {
         expect(shallow(<EditDepartmentModal errors={{}}/>)).toMatchSnapshot();
     });
-    it("expect AddDepartmentModal Snapshot", () => {
+    it("Take AddDepartmentModal Snapshot", () => {
         expect(shallow(<AddDepartmentModal errors={{}}/>)).toMatchSnapshot();
     });
 });
 
 
-describe("expect Department render", () => {
+describe("Test Department render", () => {
     it("expect DepartmentItem render with selected element", () => {
-        expect(render(<DepartmentItem departments={[]}/>)["0"].name).toEqual("table");
-        expect(render(<DepartmentItem departments={[]}/>)["0"].children[0].name).toEqual("thead");
+        expect(render(<DepartmentItem departments={[]}/>)["0"].name).toEqual("div");
+        expect(render(<DepartmentItem departments={[]}/>)["0"].children[0].name).toEqual("div");
     });
     it("expect EditDepartmentModal render with selected element", () => {
         expect(render(<EditDepartmentModal errors={{}}/>)["0"].name).toEqual("div");
